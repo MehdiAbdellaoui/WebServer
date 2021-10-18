f390f566c9c7f95c5f674933b7af7e169be5a6d5
triangle-z <51131620+triangle-z@users.noreply.github.com>
2021-10-18 08:45:32 +0200

    todo

--- WebPing.java
+++ WebPing.java
@@ -4,26 +4,27 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 public class WebPing {
-  public static void main(String[] args) {
-  
-      if (args.length != 2) {
-      	System.err.println("Usage java WebPing <server host name> <server port number>");
-      	return;
-      }	
-  
-   String httpServerHost = args[0];
-    int httpServerPort = Integer.parseInt(args[1]);
-     httpServerHost = args[0];
-      httpServerPort = Integer.parseInt(args[1]);
+	public static void main(String[] args) {
+		args = new String[2] ;
+		args[0] = "0.0.0.0" ;
+		args[1] = "80" ;
+		if (args.length != 2) {
+			System.err.println("Usage java WebPing <server host name> <server port number>");
+			return;
+		}
 
-    try {
-      InetAddress addr;      Socket sock = new Socket(httpServerHost, httpServerPort);
-      addr = sock.getInetAddress();
-      System.out.println("Connected to " + addr);
-      sock.close();
-    } catch (java.io.IOException e) {
-      System.out.println("Can't connect to " + httpServerHost + ":" + httpServerPort);
-      System.out.println(e);
-    }
-  }
+		String httpServerHost = args[0];
+		int httpServerPort = Integer.parseInt(args[1]);
+
+		try {
+			InetAddress addr;
+			Socket sock = new Socket(httpServerHost, httpServerPort);
+			addr = sock.getInetAddress();
+			System.out.println("Connected to " + addr);
+			sock.close();
+		} catch (java.io.IOException e) {
+			System.out.println("Can't connect to " + httpServerHost + ":" + httpServerPort);
+			System.out.println(e);
+		}
+	}
 }
